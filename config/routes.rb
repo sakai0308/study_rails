3a4308c1868d3e50ced2149a9e3e91cbdbabdf7e
Hidenori Sakai <sakai.systems@gmail.com>
2016-11-27 08:53:24 +0000

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 DemoApp::Application.routes.draw do
-  get "static_pages/home"
-  get "static_pages/help"
-  get "static_pages/about"
+  root  'static_pages#home'
+  match '/help',    to: 'static_pages#help',    via: 'get'
+  match '/about',   to: 'static_pages#about',   via: 'get'
+  match '/contact', to: 'static_pages#contact', via: 'get'
+
   resources :tasks
 
   # The priority is based upon order of creation: first created -> highest priority.
